2c246e643da280a87c3c8228cbc6fbc5c61255f8
Chris Oliver <excid3@gmail.com>
2022-08-31 19:48:17 -0500

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
+
+  has_one_attached :avatar
+  has_person_name
+  has_noticed_notifications
+
+  has_many :notifications, as: :recipient, dependent: :destroy
+  has_many :services
+end
---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  get '/privacy', to: 'home#privacy'
+  get '/terms', to: 'home#terms'
+authenticate :user, lambda { |u| u.admin? } do
+  mount Sidekiq::Web => '/sidekiq'
+
+  namespace :madmin do
+    resources :impersonates do
+      post :impersonate, on: :member
+      post :stop_impersonating, on: :collection
+    end
+  end
+end
+
+  resources :notifications, only: [:index]
+  resources :announcements, only: [:index]
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  root to: 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
