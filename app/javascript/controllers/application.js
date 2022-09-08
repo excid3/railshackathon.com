1d926695f040edaf7e81c0fbcd4b4d61fc4ded8f
Chris Oliver <excid3@gmail.com>
2022-09-08 16:02:35 -0500

    Clean up navbar on mobile

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -9,4 +9,11 @@ window.Stimulus   = application
 export { application }
 
 import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+application.register('autosave', Autosave)
 application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
