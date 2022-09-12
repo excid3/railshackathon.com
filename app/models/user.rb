044eb2b41fbea746324d6325bce441654342a578
Chris Oliver <excid3@gmail.com>
2022-09-12 09:38:01 -0500

    Remove notifications since we're not using them

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
 
-  has_many :notifications, as: :recipient, dependent: :destroy
   has_many :services, dependent: :destroy
   has_one :team_user, dependent: :destroy
   has_one :team, through: :team_user
 
   has_one_attached :avatar
   has_person_name
-  has_noticed_notifications
 
   validates :name, presence: true
 
