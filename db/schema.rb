7ec7a1bf087d79de89bae60ccd316717bdcabdf4
cjilbert504 <54157657+cjilbert504@users.noreply.github.com>
2022-09-14 09:57:14 -0500

    add complete attr to entries (#8)
    * add complete attr to entries
    
    * clean up ui

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_154950) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_140319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_154950) do
     t.bigint "team_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "complete"
     t.index ["team_id"], name: "index_entries_on_team_id"
   end
 
