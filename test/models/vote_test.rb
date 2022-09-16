83d760c45cc18489e7dde93b3907f984f5448926
Collin Jilbert <cjilbert504@gmail.com>
2022-09-16 12:45:22 -0500

    add test for vote validation

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "users can only have a maximum of 5 vote" do
+    user = users(:one)
+    entry = entries(:six)
+
+    new_vote = user.votes.build(entry: entry)
+    refute new_vote.valid?
+  end
+
+  test "users can update their vote positions" do
+    user = users(:one)
+    vote = user.votes.first
+
+    vote.position = 100
+    assert vote.valid?
+  end
 end
