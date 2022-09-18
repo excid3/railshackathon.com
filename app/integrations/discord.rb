6fa45ad5895900bb680ee7ebe2b94c65a2d4d955
Chris Oliver <excid3@gmail.com>
2022-09-18 16:51:58 -0500

    Add discord integration

---
+++ app/integrations/discord.rb
@@ -0,0 +1,32 @@
+class Discord
+  include Rails.application.routes.url_helpers
+  include ActionDispatch::Routing::PolymorphicRoutes
+  include ActionView::Helpers::NumberHelper
+  include JobsHelper
+
+  attr_reader :entry
+
+  def initialize(entry)
+    @entry = entry
+  end
+
+  def post
+    HTTP.post(Rails.application.credentials.dig(:discord, :new_entry_url), json: {embeds: [embeds]})
+  end
+
+  def embeds
+    embed = {
+      title: "#{entry.title} by #{entry.team.name}",
+      description: entry.description.to_plain_text.tr("\n", " ").truncate(300),
+      built_with: entry.built_with.to_plain_text.tr("\n", " ").truncate(300),
+      url: entry.website_url,
+      color: 5814783
+    }
+
+    embed
+  end
+
+  def default_url_options
+    Rails.application.config.action_mailer.default_url_options
+  end
+end
