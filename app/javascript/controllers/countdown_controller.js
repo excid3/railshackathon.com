7b2e917fb97325dd340c82e58a62ad70e4771b3c
Chris Oliver <excid3@gmail.com>
2022-09-18 10:10:02 -0500

    Update countdown

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -3,13 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = {
     date: String,
+    prefix: String,
+    suffix: String,
     refreshInterval: { type: Number, default: 1000 },
-    expiredMessage: { type: String, default: "The hackathon has started!" }
+    expiredMessage: { type: String, default: "The hackathon has ended!" }
   }
 
   connect() {
     this.endTime = new Date(this.dateValue).getTime()
-
     this.update()
     this.timer = setInterval(() => {
       this.update()
@@ -28,10 +29,11 @@ export default class extends Controller {
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    let count = []
+    let count = [this.prefixValue]
     if (days > 0) { count.push(`${days} days`) }
     if (hours > 0) { count.push(`${hours} hours`) }
     if (minutes > 0) { count.push(`${minutes} minutes`) }
+    count.push(this.suffixValue)
 
     // If the count down is finished, write some text
     if (distance < 0) {
