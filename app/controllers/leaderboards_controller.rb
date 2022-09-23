c3c078fd88a3fc249b647484bd20b7d56d9f5c53
Chris Oliver <excid3@gmail.com>
2022-09-23 08:55:17 -0500

    Only join teams

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,5 +1,5 @@
 class LeaderboardsController < ApplicationController
   def show
-    @entries = Entry.joins(team: :users).order(total_points: :desc)
+    @entries = Entry.joins(:team).order(total_points: :desc)
   end
 end
