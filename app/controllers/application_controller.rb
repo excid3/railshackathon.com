df1d58cd786111eb4573b76178adcf0b88ae401b
Kentastic <kent@hey.com>
2023-06-14 19:14:20 -0400

    linting views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   impersonates :user
-  #TODO
-  include Pundit#::Authorization
-
+  include Pundit
+  
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
