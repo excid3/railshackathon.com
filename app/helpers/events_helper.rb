36714123d74633df7b2d8528079dbe8f281e0180
Kentastic <kent@hey.com>
2023-06-14 17:08:55 -0400

    create event resources

---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,22 @@
+module EventsHelper
+  def event_date_info(event)
+    event_start_tag = content_tag(:time, event_start(event), datetime: event_html_attrs(event.start_time))
+    event_end_tag = content_tag(:time, event_end(event), datetime: event_html_attrs(event.end_time))
+    final_tag = event_start_tag + " - " + event_end_tag
+  end
+  
+  def ordinalize_event_datetime(event_datetime)
+    event_datetime.strftime("%B #{event_datetime.day.ordinalize} at %l %p #{event_datetime.localtime.zone}")
+  end
+  def event_start(event)
+    event.start_time.strftime("%B %d")
+  end
+  
+  def event_end(event)
+    event.end_time.strftime("%d, %Y")
+  end
+  
+  def event_html_attrs(event_datetime)
+     event_datetime.strftime("%Y-%m-%d")
+  end
+end
---
+++ app/models/event.rb
@@ -0,0 +1,21 @@
+class Event < ApplicationRecord
+  has_many :teams, dependent: :destroy
+  has_many :entries, through: :teams
+  
+  validates :start_time, :end_time, :theme, presence: :true
+  validates :start_time, comparison: { less_than: :end_time }
+  
+  scope :published, -> { where(published: true) }
+
+  def self.current
+    published.where("end_time > ?", Time.current).order(end_time: :asc).take
+  end
+
+  def self.previous
+    published.where("end_time < ?", Time.current).order(end_time: :desc).take
+  end
+  
+  def active?
+    end_time.after? Time.current
+  end
+end
