123ce7d7d235177b893bbc20fdd79d8c2bf7068f
Kentastic <kent@hey.com>
2023-06-15 10:07:46 -0400

    fine tunings

--- app/controllers/events/teams_controller.rb
+++ app/controllers/events/teams_controller.rb
@@ -1,8 +1,9 @@
 class Events::TeamsController < ApplicationController
   
   def index
-    @event = Event.find(params[:event_id])
-    @teams = @event.teams
+    event = Event.find(params[:event_id])
+    @event_teams = event.teams
+    @total_developers = @event_teams.sum {|team| team.team_users.size }
   rescue ActiveRecord::RecordNotFound
     redirect_to events_path, notice: "Invalid Event"
   end
