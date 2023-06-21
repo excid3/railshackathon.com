2f160807d763ec14dae79d4d766fab10487ccb36
Chris Oliver <excid3@gmail.com>
2023-06-21 09:59:18 -0500

    Sort events page newest first

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,28 +1,31 @@
 class Event < ApplicationRecord
   has_many :teams, dependent: :destroy
   has_many :entries, through: :teams
-  
+
   validates :start_time, :end_time, :title, :theme, presence: :true
   validates :start_time, comparison: { less_than: :end_time }
-  
+
   scope :published, -> { where(published: true) }
+  scope :future, -> { where("end_time >= ?", Time.current) }
+  scope :past, -> { where("end_time < ?", Time.current) }
+  scope :newest_first, -> { order(end_time: :desc) }
 
   def self.latest
     current || previous
   end
-  
+
   def self.current
-    published.where("end_time > ?", Time.current).order(end_time: :asc).take
+    future.published.order(end_time: :asc).first
   end
 
   def self.previous
-    published.where("end_time < ?", Time.current).order(end_time: :desc).take
+    past.published.newest_first.first
   end
-  
+
   def to_param
     [id, title.parameterize].join("-")
   end
-  
+
   def active?
     end_time.after? Time.current
   end
