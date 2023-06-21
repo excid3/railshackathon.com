4ed1eb54f0bfeb9a3cddeae79ed5b6bb3e2eeea7
Chris Oliver <excid3@gmail.com>
2023-06-21 10:29:33 -0500

    Add countdown helper and fix link_to_if since it never works like I expect it to

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,7 +4,7 @@ module EventsHelper
     event_end_tag = content_tag(:time, event_end(event), datetime: event_html_attrs(event.end_time))
     final_tag = event_start_tag + " - " + event_end_tag
   end
-  
+
   def ordinalize_event_datetime(event_datetime)
     event_datetime.strftime("%B #{event_datetime.day.ordinalize} at %l %p #{event_datetime.localtime.zone}")
   end
@@ -12,12 +12,16 @@ module EventsHelper
   def event_start(event)
     event.start_time.strftime("%B %d")
   end
-  
+
   def event_end(event)
     event.end_time.strftime("%d, %Y")
   end
-  
+
   def event_html_attrs(event_datetime)
      event_datetime.strftime("%Y-%m-%d")
+  end
+
+  def countdown(time, prefix:)
+    tag.span data: { controller: "countdown", countdown_date_value: time, countdown_prefix_value: prefix }
   end
 end
