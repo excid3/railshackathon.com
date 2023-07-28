a804b9f772e6e610cfdf19eda589090335afd753
Chris Oliver <excid3@gmail.com>
2023-07-28 17:41:22 -0500

    Scope votes maximum to event

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -11,15 +11,12 @@ class Vote < ApplicationRecord
 
   MAXIMUM = 5
 
-  # after_commit :update_entry_total_point
-
-
   def user_can_have_only_five_votes
     # Using size here will count the in-memory instances plus the db records
     # If you have 5 vote records in the database and try to create a 6th record
     # this validation will fail. However, if you are only updating the order of your
     # existing votes the validation will pass because there is no in-memory instance.
-    if user.votes.size > MAXIMUM
+    if user.votes.joins(:event).where(events: {id: event.id}).size > MAXIMUM
       errors.add(:base, "You've reached the #{MAXIMUM} vote limit")
     end
   end
