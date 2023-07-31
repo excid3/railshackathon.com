48f91df17029380366303664a55b9e6ac8c355ad
Kentastic <kent@hey.com>
2023-07-31 17:15:22 -0400

    6 lords a-leaping, ffiiiiivvvveee golden votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -12,11 +12,7 @@ class Vote < ApplicationRecord
   MAXIMUM = 5
 
   def user_can_have_only_five_votes
-    # Using size here will count the in-memory instances plus the db records
-    # If you have 5 vote records in the database and try to create a 6th record
-    # this validation will fail. However, if you are only updating the order of your
-    # existing votes the validation will pass because there is no in-memory instance.
-    if user.votes.where(event: event).size > MAXIMUM
+    if user.votes.where(event: event).size >= MAXIMUM
       errors.add(:base, "You've reached the #{MAXIMUM} vote limit")
     end
   end
